4c759b060e1dc51698eba86f28bf417f81773e56
--local <fadhilprawira87@gmail.com>
2024-05-19 10:28:50 +0700

    Remove variables.dart from history

--- lib/core/constants/variables.dart
+++
@@ -1,7 +0,0 @@
-class Variables {
-  static const String appName = 'PML Ship';
-  static const String apiVersion = 'v1';
-  // static const String baseUrl = 'http://10.104.88.149:8000';
-  // static const String baseUrl = 'http://192.168.1.19:8000';
-  static const String baseUrl = 'https://fadhilprawira.xyz';
-}
