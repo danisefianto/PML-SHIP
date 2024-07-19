618644a6fde395390be7972c5538d2a2b3cf445a
--local <fadhilprawira87@gmail.com>
2024-07-19 13:28:25 +0700

    change variable

---
+++ lib/core/constants/variables.dart
@@ -0,0 +1,9 @@
+class Variables {
+  static const String appName = 'PML Ship';
+  static const String baseUrl = 'http://10.107.44.141:8000';
+  // static const String baseUrl = 'https://fadhilprawira.xyz';
+  static const String documentURL = '$baseUrl/storage/documents/';
+  static const String weatherApiUrl = 'https://api.open-meteo.com/';
+  static const String weatherApiVersion = 'v1';
+  static const String imageUrl = '$baseUrl/storage';
+}
